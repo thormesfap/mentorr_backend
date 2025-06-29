9de906c4d2ea27463936e2a47b1adc8575b31f4d
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2025-06-29 09:28:27 -0300

    Alteração de database seeder para criar um usuário admin antes dos demais usuários

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders;
 
-use App\Models\Mentor;
+use Illuminate\Database\Seeder;
 use App\Models\User;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\Mentor;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,6 +23,15 @@ class DatabaseSeeder extends Seeder
             EmpresaSeeder::class,
             HabilidadeSeeder::class,
         ]);
+        $admin = User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@mentorr.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $role_admin = Role::where('name', 'role_admin')->get()->first();
+        $admin->roles()->attach($role_admin);
+        $admin->save();
+
         User::factory(20)->create();
         Mentor::factory(10)->create();
 
